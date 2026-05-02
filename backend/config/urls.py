e5709e5312ebcde55ddc81996e364543e8d4a7d6
Olly <ollyshall@gmail.com>
2026-05-02 13:18:23 +0100

    Change admin site name and ensure favicon is correct for admin site as well

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from bookings.views import AvailabilityView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('waynesworld/', admin.site.urls),
     path('api/rooms/', include('rooms.urls')),
     path('api/availability/', AvailabilityView.as_view(), name='availability'),
     path('api/bookings/', include('bookings.urls')),
